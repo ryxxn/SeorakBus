9bfddc8f48d8d047ab4adc47f43efe33ce7e7bc1
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-02-18 21:00:38 +0900

    Header 컴포넌트 분리

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = () => {
+    return (
+        <header className='mainHeader'>
+            <h1>{"설악 - 잠실 버스"}</h1>
+        </header>
+
+    )
+}
