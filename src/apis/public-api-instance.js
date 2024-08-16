64f9e795a4d3935bd6463a561a75d219414630c0
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-16 22:39:05 +0900

    chore: remove annotation

--- src/apis/public-api-instance.js
+++ src/apis/public-api-instance.js
@@ -6,7 +6,7 @@ const TIMEOUT_TIME = 10_000;
 // 공공 api 전용 axios instance
 export const publicApiInstance = axios.create({});
 
-// 취소 토큰을 생성하는 함수
+// create cancel token
 const cancelTokenSource = () => {
   const cancelToken = axios.CancelToken.source();
   return {
@@ -27,7 +27,6 @@ publicApiInstance.interceptors.request.use(
     return config;
   },
   (error) =>
-    // 요청 전 에러 처리
     Promise.reject(
       (error.response && error.response.data) || 'Something went wrong'
     )
@@ -48,11 +47,9 @@ publicApiInstance.interceptors.response.use(
   async (error) => {
     // timeout
     if (axios.isCancel(error)) {
-      // 취소된 요청은 에러로 처리하지 않음
       Promise.resolve();
     }
 
-    // 그 외의 에러는 그대로 반환
     return Promise.reject(
       (error.response && error.response.data) || 'Something went wrong'
     );
