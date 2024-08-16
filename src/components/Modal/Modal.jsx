698ab8d2bfc5d372e7d40df9e667e59125e755a7
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-16 22:26:13 +0900

    refactor: add index.js and change folder name and module scss

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,69 +1,80 @@
-import React, { useEffect, useRef } from 'react'
-import "./Modal.scss"
+import React from 'react';
+import styles from './styles.module.scss';
+import { bindClassNames } from '@/utils';
+
+// ----------------------------------------------------------------------
+
+const cn = bindClassNames(styles);
+
+// ----------------------------------------------------------------------
 
 /**
  * Modal 컴포넌트
  * background 클릭해도 닫힘.
  * @param {string} text 내용
- * @param {boolean} isOpen 
- * @param {Function} setIsOpen 
+ * @param {boolean} isOpen
+ * @param {Function} setIsOpen
  */
-export const Modal = ({
-    className,
-    isOpen,
-    setIsOpen,
-    children,
-    button,
-    closeButton,
-    buttonsDirection = "row",
+const Modal = ({
+  className,
+  isOpen,
+  setIsOpen,
+  children,
+  button,
+  closeButton,
+  buttonsDirection = 'row',
 }) => {
+  const backgroundRef = React.useRef(null);
 
-    const backgroundRef = useRef(null);
-
-    const handleClickBackground = (e) => {
-        if (e.target === backgroundRef.current) {
-            setIsOpen(false);
-        }
-    };
-
-    const handleButtonClick = (onClick) => {
-        setIsOpen(false);
-        onClick();
+  const handleClickBackground = (e) => {
+    if (e.target === backgroundRef.current) {
+      setIsOpen(false);
     }
+  };
 
-    return (
-        <>
-            <div
-                className={`modalContainer ${!isOpen ? 'hide' : ''} ${className}`}
-                ref={backgroundRef}
-                onClick={handleClickBackground}
-            >
-                <div className='modalBox'>
-                    {children}
-                    <div className="buttonsBox"
-                        style={buttonsDirection === "row" ? {} : { flexDirection: "column-reverse" }}
-                    >
-                        {
-                            closeButton &&
-                            <button
-                                className="closeButton"
-                                onClick={() => handleButtonClick(closeButton.onClick)}
-                            >
-                                {closeButton.text}
-                            </button>
-                        }
-                        {
-                            button &&
-                            <button
-                                className="normalButton"
-                                onClick={() => handleButtonClick(button.onClick)}
-                            >
-                                {button.text}
-                            </button>
-                        }
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+  const handleButtonClick = (onClick) => {
+    setIsOpen(false);
+    onClick();
+  };
+
+  return (
+    <>
+      <div
+        className={cn('modalContainer', isOpen ? '' : 'hide', className)}
+        ref={backgroundRef}
+        onClick={handleClickBackground}
+      >
+        <div className={cn('modalBox')}>
+          {children}
+          <div
+            className={cn('buttonsBox')}
+            style={
+              buttonsDirection === 'row'
+                ? {}
+                : { flexDirection: 'column-reverse' }
+            }
+          >
+            {closeButton && (
+              <button
+                className={cn('closeButton')}
+                onClick={() => handleButtonClick(closeButton.onClick)}
+              >
+                {closeButton.text}
+              </button>
+            )}
+            {button && (
+              <button
+                className={cn('normalButton')}
+                onClick={() => handleButtonClick(button.onClick)}
+              >
+                {button.text}
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
